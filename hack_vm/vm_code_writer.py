c01b8677acf087dd1849e364c12cf7d40fe05ee8
Vladimir Lisovskiy <erehonvl@gmail.com>
2018-07-06 14:31:29 +0300

    stack eq

--- hack_vm/vm_code_writer.py
+++ hack_vm/vm_code_writer.py
@@ -2,6 +2,8 @@ from vm_command_type import vm_command_type
 
 ml_lines = []
 commands = ['empty']
+equality_counter = 0
+next_counter = 0
 
 
 def prev_constant():
@@ -27,7 +29,48 @@ def write_arithmetic_command(line_def):
 
 def write_eq_command(line_def):
     if line_def['arg'][0] == 'eq':
-        ml_lines.append('D=D&A')
+        ml_lines.append('D=D-A')
+        global equality_counter
+        eq_label_true = 'EQ.TRUE.'+str(equality_counter)
+        eq_label_false = 'EQ.FALSE.'+str(equality_counter)
+        ml_lines.append('@' + eq_label_true)
+        ml_lines.append('D;JEQ')
+        ml_lines.append('@' + eq_label_false)
+        ml_lines.append('0;JMP')
+        add_symbol_link(eq_label_true)
+        set_sp_bool(True)
+        add_next_symbol()
+        add_symbol_link(eq_label_false)
+        set_sp_bool(False)
+        symb_next = add_next_symbol()
+        add_symbol_link(symb_next)
+        global next_counter
+        next_counter += 1
+        equality_counter += 1
+
+
+def add_next_symbol():
+    global next_counter
+    symb_next = 'NEXT.' + str(next_counter)
+    ml_lines.append('@' + symb_next)
+    ml_lines.append('0;JMP')
+    return symb_next
+
+
+def add_symbol_link(label):
+    ml_lines.append(get_symbol_link(label))
+
+
+def set_sp_bool(pos):
+    ml_lines.append('@SP')
+    if pos:
+        ml_lines.append('M=0')
+    else:
+        ml_lines.append('M=-1')
+
+
+def get_symbol_link(symbol):
+    return '(' + symbol + ')'
 
 
 def write_add_command(line_def):
